c13cc321e43df4553d2dc2064c0a2446dcb39080
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-18 15:59:17 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from tm import tm
+
+setup(name="tm",
+      version=tm.__version__,
+      url="https://github.com/Ethanal/tm",
+      license="MIT",
+      packages=["tm"],
+      entry_points = {
+              "console_scripts": [
+                  "tm = tm.tm:main",
+              ],
+          },
+      long_description="A tmux wrapper featuring shortcuts and session presets.")
